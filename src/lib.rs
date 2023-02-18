6b02a11811ac1a432bcde9c15edc8ee106b62aa4
kinouchi1000 <kinouchi.takahiro.tg@tut.jp>
2023-02-18 15:51:34 +0900

    [ADD] add numpy algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use ndarray::prelude::*;
+use numpy::{IntoPyArray, PyArrayDyn};
 use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
@@ -6,9 +8,24 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+fn axpy(a: f64, x: ArrayViewD<f64>, y: ArrayViewD<f64>) -> ArrayD<f64> {
+    a * &x + &y
+}
+
+#[pyfunction]
+fn axpy_py(py: Python, a: f64, x: &PyArrayDyn<f64>, y: &PyArrayDyn<f64>) -> Py<PyArrayDyn<f64>> {
+    // axpy のラッパー
+    unsafe {
+        let x = x.as_array();
+        let y = y.as_array();
+        axpy(a, x, y).into_pyarray(py).to_owned()
+    }
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn maturin_handson(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(axpy_py, m)?)?;
     Ok(())
 }
--- test/test_sum.py
+++ test/test_sum.py
@@ -1,6 +1,19 @@
-from maturin_handson import sum_as_string
-
+from maturin_handson import sum_as_string, axpy_py
+import numpy as np
 def test_add():
     a, b = 1, 2
     c = sum_as_string(a, b)
     assert c == "3"
+
+def test_axpy():
+    a = 2
+    x = np.array([[1., 1., 1.],
+                  [1., 1., 1.],
+                  [1., 1., 1.]])
+
+    y = np.array([[1., 1., 1.],
+                  [1., 1., 1.],
+                  [1., 1., 1.]])
+
+    res = axpy_py(a, x, y)
+    assert (res == a*x+y).all()
